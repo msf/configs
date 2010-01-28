bb3544028c1ba5b710c71aa05ef1176398227719
Miguel Sousa Filipe <miguel.filipe@caixamagica.pt>
2010-01-28 00:44:17 +0000

    Fix install.py for my purposes

--- install.py
+++ install.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # stolen shamelessly from http://github.com/htr/dotfiles/blob/master/install.py
 
- 
+
 from glob import glob
 import os
 import shutil
- 
+
 config_files = glob('*')
+config_files.extend( glob('.*') )
 config_files.remove('install.py') #or should I use __file__ ?
 config_files.remove('README')
- 
+
 HOME = os.path.expanduser('~/')
 CWD = os.path.abspath('.') + '/'
- 
+
 for file in config_files:
-    destfile = HOME + '.' + file
+    destfile = HOME + file
     if os.path.exists(destfile):
         if os.path.islink(destfile):
             os.unlink(destfile)
