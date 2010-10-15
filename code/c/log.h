0a575a3fb4f0a6932dd4763b89c6cbd8ed3b0b89
Miguel Sousa Filipe <miguel.filipe@caixamagica.pt>
2010-10-15 15:09:54 +0100

    c: add random, mask, log, timer

---
+++ code/c/log.h
@@ -0,0 +1,36 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+#include "registry.h"
+
+#define ERROR(_log_,_func_x_,_str_x_) {\
+        log_print( (_log_), "%s:%d ERROR: %s failed in %s.\n",\
+         __FILE__, __LINE__, (_func_x_), (_str_x_) ); \
+	}
+
+
+typedef struct log_type {
+    FILE *logFile;
+    char *logBuf;
+    int verbose;
+    int procID;
+    int iterNum;
+    int simulNum;
+} log_t;
+
+log_t *new_log(registry *, int proc_id);
+
+void delete_log(log_t *l);
+
+void log_print(log_t *l, char *fmt, ...);
+
+void log_iteration_number(log_t *l, int iterNum);
+void log_simulation_number(log_t *l, int simulNum);
+void log_message(log_t *l, int level, char* message);
+void log_action_time(log_t *l, int level, char* action, float time);
+void log_string(log_t *l, char* dump);
+void log_result(log_t *l, int level, char* name, double value);
+void log_separator(log_t *l);
+void log_close(log_t *l);
+
+#endif
