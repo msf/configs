41a2f0c20acc02714436c8625823fc04d4a6b49d
Miguel Sousa Filipe <miguel.filipe@caixamagica.pt>
2010-10-27 23:50:16 +0100

    tweak cpu_bind

--- code/c/cpu_bind.c
+++ code/c/cpu_bind.c
@@ -8,11 +8,33 @@
 #include <stdio.h>
 #include <sched.h>
 
+int set_cpu_affinity(pid_t pid, unsigned long new_mask)
+{
+	unsigned long cur_mask;
+	unsigned int len = sizeof(new_mask);
+
+	if (sched_getaffinity(pid, len, (cpu_set_t *) &cur_mask) < 0) {
+		perror("sched_getaffinity");
+		return -1;
+	}
+	printf("pid %d's old affinity: %08lx\n", pid, cur_mask);
+
+	if (sched_setaffinity(pid, len, (cpu_set_t *) &new_mask)) {
+		perror("sched_setaffinity");
+		return -1;
+	}
+
+	if (sched_getaffinity(pid, len, (cpu_set_t *) &cur_mask) < 0) {
+		perror("sched_getaffinity");
+		return -1;
+	}
+	printf(" pid %d's new affinity: %08lx\n", pid, cur_mask);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	unsigned long new_mask;
-	unsigned long cur_mask;
-	unsigned int len = sizeof(new_mask);
 	pid_t pid;
 
 	if (argc != 3) {
@@ -21,26 +43,7 @@ int main(int argc, char *argv[])
 	}
 
 	pid = atol(argv[1]);
-	sscanf(argv[2], "%08lx", &new_mask);
+    new_mask = atol(argv[2]);
 
-	if (sched_getaffinity(pid, len, &cur_mask) < 0) {
-		perror("sched_getaffinity");
-		return -1;
-	}
-
-	printf("pid %d's old affinity: %08lx\n", pid, cur_mask);
-
-	if (sched_setaffinity(pid, len, &new_mask)) {
-		perror("sched_setaffinity");
-		return -1;
-	}
-
-	if (sched_getaffinity(pid, len, &cur_mask) < 0) {
-		perror("sched_getaffinity");
-		return -1;
-	}
-
-	printf(" pid %d's new affinity: %08lx\n", pid, cur_mask);
-
-	return 0;
+	return set_cpu_affinity(pid, new_mask);
 }
