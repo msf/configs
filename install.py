1a28ed7351eb421094eb2cd1b0499eb39dcd9e81
Miguel Filipe <miguel.filipe@gmail.com>
2019-08-28 22:41:40 +0100

    run black on install.py

--- install.py
+++ install.py
@@ -4,20 +4,20 @@ from glob import glob
 import os
 import shutil
 
-HOME = os.path.expanduser('~/')
-CWD = os.path.abspath('.') + '/'
+HOME = os.path.expanduser("~/")
+CWD = os.path.abspath(".") + "/"
 
 
 def copyFiles():
-    config_files = glob('*')
-    config_files.extend( glob('.*') )
+    config_files = glob("*")
+    config_files.extend(glob(".*"))
     dont_symlink = [
-        'install.py', #or should I use __file__ ?
-        'README',
-        '.git',
-        'awesome',
-        ]
-    dont_symlink.extend( glob('.*swp') ) # temp edit files
+        "install.py",  # or should I use __file__ ?
+        "README",
+        ".git",
+        "awesome",
+    ]
+    dont_symlink.extend(glob(".*swp"))  # temp edit files
 
     for f in dont_symlink:
         config_files.remove(f)
@@ -28,36 +28,37 @@ def copyFiles():
         if os.path.exists(destfile) or os.path.islink(destfile):
             removePath(destfile)
         try:
-            os.symlink(sourcefile,destfile)
+            os.symlink(sourcefile, destfile)
             print("symlinked %s" % file)
         except OSError as ex:
             print("failed on %s -> %s, %s" % (sourcefile, destfile, ex))
+
 
 def removePath(destfile):
     # REMOVE if dest exists
     if os.path.islink(destfile):
         os.unlink(destfile)
     elif os.path.isdir(destfile):
-        shutil.rmtree(destfile,True,True)
+        shutil.rmtree(destfile, True, True)
     else:
         os.remove(destfile)
 
+
 def copyConfigDirs():
-    ''' into .config directory stuff '''
-    configdirs = [ 'awesome'
-        ,
-        ]
+    """ into .config directory stuff """
+    configdirs = ["awesome"]
 
     destdir = HOME + ".config/"
     if not os.path.exists(destdir):
         os.mkdir(destdir)
 
-    for di in configdirs :
+    for di in configdirs:
         destpath = destdir + di
         if os.path.exists(destpath) or os.path.islink(destpath):
             removePath(destpath)
-        os.symlink( CWD + di, destpath )
+        os.symlink(CWD + di, destpath)
         print("symlinked directory %s" % destpath)
+
 
 copyFiles()
 copyConfigDirs()
