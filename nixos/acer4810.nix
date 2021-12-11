b4dd8652a11646c27708c8c4939944dd34df8355
Miguel Filipe <miguel.filipe@gmail.com>
2021-12-11 17:38:26 +0000

    nixos: acer4810, update nix

--- nixos/acer4810.nix
+++ nixos/acer4810.nix
@@ -15,37 +15,51 @@
     cpu.intel.updateMicrocode = true;
   };
 
+  boot.supportedFilesystems = [ "zfs" "btrfs" ];
+  boot.zfs.enableUnstable = true;
   boot.kernelParams = [
     "mitigations=off" # old b0x, need moar sp33d
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.grub.device = "/dev/sda";
-  boot.supportedFilesystems = [ "btrfs" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl."vm.swapiness" = 60;
 
   zramSwap = {
     enable = true;
-    memoryPercent = 30;
+    memoryPercent = 40;
     numDevices = 1;
     algorithm = "zstd";
   };
 
-  networking.hostName = "acer4810"; # Define your hostname.
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+  networking.hostId = "b05e6b14";  # for ZFS
+  networking.hostName = "margiehamilton"; # Define your hostname.
+  # Set your time zone.
+  time.timeZone = "UTC";
 
-  networking.interfaces.enp1s0.useDHCP = true;
-  networking.interfaces.wlp2s0.useDHCP = true;
-
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.interfaces.enp2s0f0.useDHCP = true;
+  #networking.interfaces.enp2s0f0.ipv4.addresses = [ {
+  #  address = "192.168.1.20";
+  #  prefixLength = 24;
+  #} ];
+  #networking.defaultGateway = "192.168.1.1";
   networking.nameservers = [ "8.8.8.8" "1.1.1.1"];
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = false;  # using network manager
 
   networking.extraHosts =
   ''
-  100.119.38.108  hopper-tail
-  100.89.241.6    acer-tail
-  100.99.150.19   lovelace-tail
+  100.119.216.56  grace-tail
   100.67.77.31    margie-tail
-  100.121.57.66   curie-tail
+  100.77.156.119  kamala-tail
+  100.92.53.58    hopper-tail
+  100.93.239.53   curie-tail
+  100.94.188.127  lovelace-tail
   '';
 
   # Configure network proxy if necessary
@@ -57,18 +71,17 @@
   networking.firewall.allowedTCPPorts = [ 22  5001 22000 21027];
   networking.firewall.allowedUDPPorts = [ 5001 5002 22000 21027];
   networking.firewall.allowPing = true;
-  # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
   # Select internationalisation properties.
-  console.useXkbConfig = true;
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    # Adds terminus_font for people with HiDPI displays
+    packages = options.console.packages.default ++ [ pkgs.terminus_font ];
   };
-
-  # Set your time zone.
-  time.timeZone = "UTC";
 
   # List packages installed in system profile. To search, run:
   environment = {
@@ -88,15 +101,15 @@
        git
        gnumake
        go
-       hdparm
        google-chrome
+       hdparm
        htop
        iotop
        lm_sensors
        lshw
        lsof
        lxappearance
-       meld
+       mbuffer
        ncdu
        parted
        pciutils
@@ -106,9 +119,8 @@
        rclone
        restic
        rxvt_unicode
+       sanoid
        smartmontools
-       syncthing
-       sysstat
        syncthing
        sysstat
        tmux
@@ -118,6 +130,7 @@
        weechat
        wget
        zfstools
+       #zoom-us
        zsh
        zstd
      ];
@@ -128,16 +141,25 @@
 
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
+  programs.dconf.enable = true; #gnome
 
-  security.pam.loginLimits = [{
+  security.pam.loginLimits = [
+  {
     domain = "*";
     type = "hard";
     item = "nofile";
     value = "65535";
-  }];
+  }
+  {
+    domain = "*";
+    type = "hard";
+    item = "nproc";
+    value = "1049600";
+  }
+  ];
 
   fonts = {
-    enableFontDir = true;
+    fontDir.enable = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
       google-fonts
@@ -159,12 +181,26 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.permitRootLogin = "no";
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "prohibit-password";
+    passwordAuthentication = false;
+  };
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMi8xVr7C/qB+DGIGa07Hm9uv0pTKZ8qbX8DywAteaXP root@hopper" ];
+
   services.timesyncd.enable = true;
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoSnapshot = {
+    enable = true;
+    monthly = 4;  # default is 12
+    weekly = 4;
+    daily = 4;  # default is 7
+    hourly = 4; # default is 24
+    frequent = 4;
+  };
 
   services.syncthing = {
-    enable = true;
+    enable = false;
     user = "miguel";
     dataDir = "/media/simple/syncthing/";
     configDir = "/home/miguel/.config/syncthing";
@@ -175,15 +211,21 @@
   services.fwupd.enable = true;
   services.tailscale.enable = true;
 
+  programs.sway.enable = true;
+  xdg.portal.wlr.enable = true;
+  services.pipewire.enable = true;
+
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
     layout = "us";
-    videoDrivers = [ "intel" ];
     libinput.enable = true;  # Enable touchpad support.
     xkbOptions = "eurosign:e ctrl:nocaps";
 
     displayManager.defaultSession = "none+i3";
+    desktopManager.gnome3.enable = false;
+#    displayManager.gdm.enable = true;
+#    displayManager.gdm.wayland = false;
 
     windowManager.i3 = {
         enable = true;
@@ -206,19 +248,27 @@
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" "python" "man" "docker" "golang" "kubectl" ];
+    theme = "agnoster";
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.miguel = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "docker"]; # Enable ‘sudo’ for the user.
     uid = 1000;
     shell = "/run/current-system/sw/bin/zsh";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeH/MddmSVsqKwTR8ys07HMW/DDDAYdsm9/lYM6hd1X miguel.filipe@2020" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCZsY3qNOZP4uL+baYJ+B2lc6SEYWnJeKKPhwZ7azhO/RleAb3SsZ7452ktvCY1YE2fAsHwgHYrZEAXj8sD1DoDUMUWael2MAAzTdnPJWriINO5QeZ1WrSLaFHb5eQ4fUMpidCmFOnEWOl9MUopeTrOgLElKoAaq9mWQvBo3VtRXH4bk4/dkCWhYuI8rpXk9w+oNhTgFr9NumSnRIFDwKazNwZFjNxt0actwKanebg7lDQabTCGc3CuU59YGiYjQmgBpvb7mkQJi5grGdCg0uFeee2NlsSBUmmxBG+OLgrtjFXpbcm2H3IgBxQRRUnN2dho2sZW2c7tV4queKmSVsEtyEQcSpc5NQZrIFE6tVEeXHhfxFtGe2qmEgX6Zmh+/TgrGTJWocsQvvuRaCrJ5jTQkYHl/9rgIoSBc5NtUL/duVlA4DzvUOUsjDyU00WaTAHB0pm767ZICyN+7Zkb3o934+hreYzMszvL60sit1V4y8ORLplUJvGhkNHrljOrtp2VVtluWEPxJLENbiiUMDB6PqQI8c4vEx4BVvFeWaPJcAZLc2y9ZX5w8R6fl2f5VWXiGbjJl4xfTquSWa3YbC//x12KFyOvMzQCctCX6fgvgEg9oGig9Xg3fEoN/R26JBjbKbCeZI5UWSIOZrrTEo50icUsUR6AweIVQ1q2IV5NQ== miguel.filipe@gmail.com" ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
   system.autoUpgrade.enable = true;  # incremental updates are good
   system.autoUpgrade.allowReboot = false;  # not that crazy
@@ -233,7 +283,7 @@
 
   # servers/services inside containers
   environment.etc = {
-    "telegraf.conf" = {
+    "telegraf/telegraf.conf" = {
       mode = "0440";
       text = ''
         [global_tags]
@@ -249,10 +299,10 @@
           hostname = "acer4810"
           omit_hostname = false
         [[outputs.influxdb_v2]]
-          urls = ["http://hopper-tail:8086"]
-          token = "GnK3erFQGnB3aLonK6mCiIRYTGenl4ShRGdxr7M3E6b2yzl51shxHUR7gJdTagJ094Vpf8fJzzotCWwhSxclHA=="
+          urls = ["http://grace-tail:8086"]
+	  token = "6fm31K9UVWC2o0oADBWg_broHVpdV9egDoj51mMGy-pYvRNAPBB475qjWRTb-8N66mTOsXbeQcM8YVvzwxrLNw=="
           organization = "casa"
-          bucket = "hopper"
+          bucket = "alfeizerao"
         [[inputs.cpu]]
           percpu = true
           totalcpu = true
@@ -273,17 +323,35 @@
     };
   };
 
+  # This is required by podman to run containers in rootless mode.
+  security.unprivilegedUsernsClone = config.virtualisation.containers.enable;
+
   virtualisation = {
     podman = {
       enable = true;
-      dockerCompat = true;
+      extraPackages = [ pkgs.zfs ];
+      #dockerCompat = true;
     };
     oci-containers = {
-      backend = "podman";
+      backend = "docker";
       containers = {
         telegraf = {
           image = "telegraf:1.17-alpine";
-          volumes = [ "/etc/telegraf.conf:/etc/telegraf/telegraf.conf:ro" ];
+          extraOptions = ["--network=host"];
+          volumes = [
+             "/:/hostfs:ro"
+             "/etc:/hostfs/etc:ro"
+             "/proc:/hostfs/proc:ro"
+             "/sys:/hostfs/sys:ro"
+             "/var/run/utmp:/var/run/utmp:ro"
+             "/etc/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro"
+          ];
+          environment = {
+            HOST_ETC = "/hostfs/etc";
+            HOST_PROC = "/hostfs/proc";
+            HOST_SYS = "/hostfs/sys";
+            HOST_MOUNT_PREFIX = "/hostfs";
+          };
         };
       };
     };
