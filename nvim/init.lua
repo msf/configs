7e9ee90919f6bcdfe2c52c4e75785464922aa945
Miguel Filipe <miguel.filipe@gmail.com>
2024-05-19 17:41:08 +0100

    update my nvim on lovelace

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
-vim.g.mapleader = ","
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
@@ -12,7 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 		lazypath,
 	})
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 require("sets")
---
+++ nvim/lua/plug.lua
@@ -0,0 +1,8 @@
+return require('packer').startup(function()
+    -- other plugins...
+
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+
+   -- other plugins...
+end)
