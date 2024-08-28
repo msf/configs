43d5f34be1159820047d27d0f69932c20607b671
Miguel Filipe <miguel.filipe@gmail.com>
2024-08-28 13:43:40 -0700

    nvim gruvbox, autformat on save buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,27 +2,27 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("sets")
 
 require("lazy").setup("plugins", {
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
 })
 
-vim.cmd([[colorscheme desert]])
+vim.cmd([[colorscheme gruvbox]])
 
 require("sets")
 require("mappings")
@@ -39,3 +39,6 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Automatically format on save
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
