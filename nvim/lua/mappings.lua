421e01a75f55a5cd960340ffcd9d9abdc6f71f5a
Miguel Filipe <miguel.filipe@gmail.com>
2024-12-27 15:44:36 +0000

    update nvim, start using parrot for AI assistance, WIP

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -83,11 +83,3 @@ end, opts)
 vim.keymap.set("n", "<leader>fb", telescope.buffers, opts)
 vim.keymap.set("n", "<leader>fh", telescope.help_tags, opts)
 
--- Neotest
-local jester = require("jester")
-vim.keymap.set("n", "<leader>tt", jester.run, opts)
-vim.keymap.set("n", "<leader>tl", jester.run_last, opts)
-vim.keymap.set("n", "<leader>tf", jester.run_file, opts)
-vim.keymap.set("n", "<leader>tdt", jester.debug, opts)
-vim.keymap.set("n", "<leader>tdl", jester.debug_last, opts)
-vim.keymap.set("n", "<leader>tdf", jester.debug_file, opts)
