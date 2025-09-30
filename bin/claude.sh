921c6a8d6609ab239762def107badf9e2fba850e
Miguel Filipe <miguel.filipe@gmail.com>
2025-09-30 11:58:15 +0100

    fix sonnet4.5 uri

--- bin/claude.sh
+++ bin/claude.sh
@@ -2,7 +2,7 @@
 
 export CLAUDE_CODE_USE_BEDROCK=1
 export AWS_REGION=us-east-1
-export ANTHROPIC_MODEL='arn:aws:bedrock:us-east-1:118330671040:inference-profile/anthropic.claude-sonnet-4-5-20250929-v1:0'
+export ANTHROPIC_MODEL='arn:aws:bedrock:us-east-1:118330671040:inference-profile/us.anthropic.claude-sonnet-4-5-20250929-v1:0'
 export ANTHROPIC_SMALL_FAST_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/eu.anthropic.claude-3-5-haiku-20241022-v1:0'
 
 claude $*
