59e1522d5c2affd0ba0dd4f3b856e9567813b0d4
Miguel Filipe <miguel.filipe@gmail.com>
2026-03-05 11:06:05 +0000

    claude 4.6 models

--- bin/claude.sh
+++ bin/claude.sh
@@ -4,15 +4,11 @@
 export CLAUDE_CODE_USE_BEDROCK=1
 export AWS_REGION=eu-west-1
 
-# Recommended output token settings for Bedrock
-#export CLAUDE_CODE_MAX_OUTPUT_TOKENS=4096
-#export MAX_THINKING_TOKENS=1024
-
-export ANTHROPIC_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/global.anthropic.claude-sonnet-4-5-20250929-v1:0'
+export ANTHROPIC_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/global.anthropic.claude-sonnet-4-6'
 export ANTHROPIC_SMALL_FAST_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/global.anthropic.claude-haiku-4-5-20251001-v1:0'
 
 if [[ "$1" == "opus" ]]; then
-    export ANTHROPIC_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/global.anthropic.claude-opus-4-5-20251101-v1:0'
+    export ANTHROPIC_MODEL='arn:aws:bedrock:eu-west-1:118330671040:inference-profile/global.anthropic.claude-opus-4-6-v1'
     shift
 fi
 
