7a02da9b3ef2b5756af0e896d89d30f0db0cf577
Miguel Filipe <miguel.filipe@gmail.com>
2026-08-25 01:57:51 +0100

    nixos: upgrade margiehamilton to 26.05

--- nixos/margiehamilton-config.nix
+++ nixos/margiehamilton-config.nix
@@ -2,64 +2,24 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, options, ... }:
+{ pkgs, options, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
-  hardware = {
-    enableRedistributableFirmware = true;
-    cpu.intel.updateMicrocode = true;
-  };
-
-  boot.supportedFilesystems = [ "zfs" "btrfs" ];
-  boot.zfs.enableUnstable = true;
-  boot.kernelParams = [
-    "zfs.zfs_arc_max=3221225472"  # 3GB
-    "mitigations=off"  # old b0x, need moar sp33d
-  ];
+  imports = [ ./margiehamilton-hw-config.nix ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernel.sysctl."vm.swapiness" = 60;
 
-  # premature to enable this..
-  #zramSwap = {
-  #  enable = true;
-  #  memoryPercent = 20;
-  #  numDevices = 1;
-  #  algorithm = "zstd";
-  #};
-
-  networking.hostId = "b05e6b14";  # for ZFS
-  networking.hostName = "margiehamilton"; # Define your hostname.
-  # Set your time zone.
   time.timeZone = "UTC";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.interfaces.enp2s0f0.useDHCP = true;
-  #networking.interfaces.enp2s0f0.ipv4.addresses = [ {
-  #  address = "192.168.1.20";
-  #  prefixLength = 24;
-  #} ];
-  #networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [ "8.8.8.8" "1.1.1.1"];
-  networking.networkmanager.enable = true;
-  networking.wireless.enable = false;  # using network manager
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 
   networking.extraHosts =
   ''
-  100.119.216.56  grace-tail
+  100.119.216.56  hopper-tail
   100.67.77.31    margie-tail
   100.77.156.119  kamala-tail
-  100.92.53.58    hopper-tail
   100.93.239.53   curie-tail
   100.94.188.127  lovelace-tail
   '';
@@ -90,39 +50,41 @@
      variables = {
        EDITOR = "vim";
      };
+     sessionVariables = {
+       BROWSER = "firefox";
+       MOZ_ENABLE_WAYLAND = "1";
+     };
      systemPackages = with pkgs; [
        atop
+       dool
+       (runCommand "dstat" { } ''
+         mkdir -p "$out/bin"
+         ln -s "${dool}/bin/dool" "$out/bin/dstat"
+       '')
        awscli
        btrfs-progs
-       docker
-       dstat
        file
-       firefox
        fwupd
        gcc
        git
-       gnome3.adwaita-icon-theme
-       gnome3.gnome-power-manager
        gnumake
        go
-       google-chrome
        hdparm
        htop
        iotop
        lm_sensors
        lshw
        lsof
-       lxappearance
        mbuffer
        ncdu
+       neovim
        parted
        pciutils
-       podman
        powertop
        python3
        rclone
        restic
-       rxvt_unicode
+       xterm
        sanoid
        smartmontools
        syncthing
@@ -145,7 +107,6 @@
 
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
-  programs.dconf.enable = true; #gnome
 
   security.pam.loginLimits = [
   {
@@ -165,7 +126,7 @@
   fonts = {
     fontDir.enable = true;
     enableGhostscriptFonts = true;
-    fonts = with pkgs; [
+    packages = with pkgs; [
       google-fonts
       inconsolata
       liberation_ttf
@@ -173,7 +134,6 @@
       source-code-pro
       terminus_font
       ttf_bitstream_vera
-      ubuntu_font_family
     ];
   };
 
@@ -187,8 +147,10 @@
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
-    permitRootLogin = "prohibit-password";
-    passwordAuthentication = false;
+    settings = {
+      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = false;
+    };
   };
   users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMi8xVr7C/qB+DGIGa07Hm9uv0pTKZ8qbX8DywAteaXP root@hopper" ];
 
@@ -215,33 +177,38 @@
   services.fwupd.enable = true;
   services.tailscale.enable = true;
 
-  programs.sway.enable = true;
-  xdg.portal.wlr.enable = true;
-  services.pipewire.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
+  programs.sway = {
     enable = true;
-    layout = "us";
-    libinput.enable = true;  # Enable touchpad support.
-    xkbOptions = "eurosign:e ctrl:nocaps";
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      alacritty
+      brightnessctl
+      grim
+      pulseaudio
+      swayidle
+      swaylock
+      wmenu
+    ];
+  };
 
-    displayManager.defaultSession = "none+i3";
-    desktopManager.gnome3.enable = false;
-#    displayManager.gdm.enable = true;
-#    displayManager.gdm.wayland = false;
+  services.greetd = {
+    enable = true;
+    useTextGreeter = true;
+    settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd sway";
+  };
 
-    windowManager.i3 = {
-        enable = true;
-        # package = pkgs.i3-gaps;
-        extraPackages = with pkgs; [
-            dmenu
-            i3status
-            i3lock
-            i3blocks
-            feh
-        ];
+  programs.firefox = {
+    enable = true;
+    policies.SearchEngines = {
+      Default = "DuckDuckGo";
+      DefaultPrivate = "DuckDuckGo";
     };
+    preferences = {
+      "media.av1.enabled" = false;
+      "media.ffmpeg.vaapi.enabled" = true;
+      "media.mediasource.vp9.enabled" = false;
+    };
+    preferencesStatus = "default";
   };
 
 
@@ -249,13 +216,22 @@
   services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" "python" "man" "docker" "golang" "kubectl" ];
+    theme = "agnoster";
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.miguel = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker"]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
     uid = 1000;
     shell = "/run/current-system/sw/bin/zsh";
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeH/MddmSVsqKwTR8ys07HMW/DDDAYdsm9/lYM6hd1X miguel.filipe@2020" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCZsY3qNOZP4uL+baYJ+B2lc6SEYWnJeKKPhwZ7azhO/RleAb3SsZ7452ktvCY1YE2fAsHwgHYrZEAXj8sD1DoDUMUWael2MAAzTdnPJWriINO5QeZ1WrSLaFHb5eQ4fUMpidCmFOnEWOl9MUopeTrOgLElKoAaq9mWQvBo3VtRXH4bk4/dkCWhYuI8rpXk9w+oNhTgFr9NumSnRIFDwKazNwZFjNxt0actwKanebg7lDQabTCGc3CuU59YGiYjQmgBpvb7mkQJi5grGdCg0uFeee2NlsSBUmmxBG+OLgrtjFXpbcm2H3IgBxQRRUnN2dho2sZW2c7tV4queKmSVsEtyEQcSpc5NQZrIFE6tVEeXHhfxFtGe2qmEgX6Zmh+/TgrGTJWocsQvvuRaCrJ5jTQkYHl/9rgIoSBc5NtUL/duVlA4DzvUOUsjDyU00WaTAHB0pm767ZICyN+7Zkb3o934+hreYzMszvL60sit1V4y8ORLplUJvGhkNHrljOrtp2VVtluWEPxJLENbiiUMDB6PqQI8c4vEx4BVvFeWaPJcAZLc2y9ZX5w8R6fl2f5VWXiGbjJl4xfTquSWa3YbC//x12KFyOvMzQCctCX6fgvgEg9oGig9Xg3fEoN/R26JBjbKbCeZI5UWSIOZrrTEo50icUsUR6AweIVQ1q2IV5NQ== miguel.filipe@gmail.com" ];
@@ -277,8 +253,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.11"; # Did you read the comment?
-  system.autoUpgrade.enable = true;  # incremental updates are good
-  system.autoUpgrade.allowReboot = false;  # not that crazy
+  system.autoUpgrade.enable = false; # Re-enable after the staged upgrade.
 
   # Clean up packages after a while
   nix.gc = {
@@ -290,84 +265,33 @@
 
   # servers/services inside containers
   environment.etc = {
-    "telegraf/telegraf.conf" = {
-      mode = "0440";
-      text = ''
-        [global_tags]
-        [agent]
-          interval = "10s"
-          round_interval = true
-          metric_batch_size = 1000
-          metric_buffer_limit = 10000
-          collection_jitter = "0s"
-          flush_interval = "10s"
-          flush_jitter = "0s"
-          precision = ""
-          hostname = "margie"
-          omit_hostname = false
-        [[outputs.influxdb_v2]]
-          urls = ["http://grace-tail:8086"]
-	  token = "6fm31K9UVWC2o0oADBWg_broHVpdV9egDoj51mMGy-pYvRNAPBB475qjWRTb-8N66mTOsXbeQcM8YVvzwxrLNw=="
-          organization = "casa"
-          bucket = "alfeizerao"
-        [[inputs.cpu]]
-          percpu = true
-          totalcpu = true
-          collect_cpu_time = false
-          report_active = false
-        [[inputs.disk]]
-          ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
-        [[inputs.diskio]]
-        [[inputs.kernel]]
-        [[inputs.mem]]
-        [[inputs.processes]]
-        [[inputs.swap]]
-        [[inputs.system]]
-        [[inputs.zfs]]
-          poolMetrics = true
-        [[inputs.sensors]]
-      '';
-    };
+    "sway/config.d/terminal.conf".text = ''
+      bindsym $mod+Return exec alacritty
+    '';
   };
-
-  # This is required by podman to run containers in rootless mode.
-  security.unprivilegedUsernsClone = config.virtualisation.containers.enable;
 
   virtualisation = {
     podman = {
       enable = true;
-      extraPackages = [ pkgs.zfs ];
       dockerCompat = true;
+    };
+    containers = {
+      enable = true;
+      storage.settings.storage = {
+        driver = "vfs"; # ponytail: use overlay storage if image churn makes vfs materially slow.
+        runroot = "/run/containers/storage";
+        graphroot = "/var/lib/containers/storage";
+      };
     };
     oci-containers = {
       backend = "podman";
       containers = {
-        telegraf = {
-          image = "telegraf:1.17-alpine";
-          extraOptions = ["--network=host"];
-          volumes = [
-             "/:/hostfs:ro"
-             "/etc:/hostfs/etc:ro"
-             "/proc:/hostfs/proc:ro"
-             "/sys:/hostfs/sys:ro"
-             "/var/run/utmp:/var/run/utmp:ro"
-             "/etc/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro"
-          ];
-          environment = {
-            HOST_ETC = "/hostfs/etc";
-            HOST_PROC = "/hostfs/proc";
-            HOST_SYS = "/hostfs/sys";
-            HOST_MOUNT_PREFIX = "/hostfs";
-          };
-        };
         kostal2influx = {
-          image = "quay.io/msf/kostal2influx:v0.2";
+          image = "ghcr.io/msf/kostal2influx:v0.6";
           user = "nobody:nogroup";
           extraOptions = ["--network=host"];
           environment = {
-            INFLUXTOKEN = "6fm31K9UVWC2o0oADBWg_broHVpdV9egDoj51mMGy-pYvRNAPBB475qjWRTb-8N66mTOsXbeQcM8YVvzwxrLNw==";
-            INFLUXHOST = "grace-tail";
-            KOSTALHOST = "192.168.0.11";
+            VM_HOST = "hopper";
           };
         };
       };
