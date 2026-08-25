1e9ce5c7f11951665134cd0ea4b1945db5931763
Miguel Filipe <miguel.filipe@gmail.com>
2026-08-25 01:07:10 +0100

    nvim: remove obsolete plugin configs

--- vim-plug-for-neovim.sh
+++
@@ -1,5 +0,0 @@
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-echo "symlink config dir for nvim"
-ln -s $(pwd)/nvim ~/.config/nvim
